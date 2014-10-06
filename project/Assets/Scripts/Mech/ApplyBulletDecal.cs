f83324afca419d333146b1f808b1f809af9c0ae7
dylan <dwyllace@gmail.com>
2014-10-06 17:52:53 -0400

    Added mech folder to scripts because I forgot that whoops

---
+++ project/Assets/Scripts/Mech/ApplyBulletDecal.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyBulletDecal : MonoBehaviour {
+
+	public GameObject decal;
+	public float offset = 0.01f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void hasBeenShot(RaycastHit info)
+	{
+		//tells you what's shot
+		Debug.Log ("I'm Shot", gameObject);
+
+		Quaternion rotation = Quaternion.LookRotation(info.normal); 
+		//gets the shot location and adds the decal(red squyare) as well sets it as a child of the wall therefor moving with the wall
+		//the offset moves the texture slightly out as to not clip
+		GameObject instance = Instantiate(decal,info.point + info.normal * offset ,rotation)as GameObject;
+		instance.transform.parent = transform;
+	}
+}
