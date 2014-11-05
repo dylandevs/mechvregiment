653c4cf74425fd27ca99f7c5403aec0a5f4b1e8d
dylan <dwyllace@gmail.com>
2014-11-05 16:02:26 -0500

    Adding the prototype networking project for testing RPC communication

---
+++ experimental/networking/Assets/DataGet.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataGet : MonoBehaviour {
+
+	private int port = 25000;
+    private string _messageLog = "Bootering server...";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void Awake(){
+		if (Network.peerType == NetworkPeerType.Disconnected){
+            Network.InitializeServer(10, port, false);
+        }
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	 public void OnGUI() {
+        GUI.Label(new Rect(100, 100, 150, 25), ""+Network.peerType);
+        if (Network.peerType == NetworkPeerType.Server) {
+            GUI.Label(new Rect(100, 125, 150, 25), "Clients attached: " + Network.connections.Length);
+ 
+            if (GUI.Button(new Rect(100, 150, 150, 25), "Quit server")) {
+                AddMessage("Quitting...");
+                Network.Disconnect();
+                Application.Quit();
+            }
+            if (GUI.Button(new Rect(100, 175, 150, 25), "Send hi to client"))
+                SendInfoToClient();
+        }
+        GUI.TextArea(new Rect(275, 100, 300, 300), _messageLog);
+
+    }
+    /*
+    [RPC]
+    void OnPlayerConnected(NetworkPlayer player) {
+        AskClientForInfo(player);
+        AddMessage("Player connected. Now at "+Network.connections.Length);
+    }*/
+    void OnPlayerConnected(NetworkPlayer player) {
+        Debug.Log("Player connected");
+    }
+    [RPC]
+    void StartLogin() {
+    }
+    [RPC]
+    void AskClientForInfo(NetworkPlayer player) {
+        networkView.RPC("SetPlayerInfo", player, player);
+    }
+    void AddMessage(string message){
+        _messageLog += message + "\n";
+    }
+    [RPC]
+    void ReceiveInfoFromClient(string someInfo) {
+        AddMessage(someInfo);
+    }
+ 
+    string someInfo = "Server: hello client";
+    [RPC]
+    void SendInfoToClient() {
+        networkView.RPC("ReceiveInfoFromServer", RPCMode.Others, someInfo);
+    }
+ 
+    // fix RPC errors
+    [RPC]
+    void SendInfoToServer() { }
+    [RPC]
+    void SetPlayerInfo(NetworkPlayer player) { }
+    [RPC]
+    void ReceiveInfoFromServer(string someInfo) { }
+}
---
+++ experimental/networking/Assets/DataSend.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataSend : MonoBehaviour {
+ 
+    public string serverIP = "127.0.0.1";
+    public int port = 25000;
+    private string _messageLog = "";
+    string someInfo = "";
+    private NetworkPlayer _myNetworkPlayer;
+
+    void Start(){
+    	AddMessage("Starting client to connect to " + serverIP + ":" + port + ".");
+    }
+ 
+    void OnGUI() {
+    	GUI.Label(new Rect(100, 100, 150, 25), ""+Network.peerType);
+        if (Network.peerType == NetworkPeerType.Disconnected) {
+            if (GUI.Button(new Rect(100, 125, 150, 25), "connect")) {
+                AddMessage("Connecting...");
+                Network.Connect(serverIP, port);
+            }
+        } else {
+            if (Network.peerType == NetworkPeerType.Client) {
+ 
+                if (GUI.Button(new Rect(100, 125, 150, 25), "disconnect"))
+                    Network.Disconnect();
+ 
+                if (GUI.Button(new Rect(100, 150, 150, 25), "say hi to server"))
+                    SendInfoToServer();
+            }
+        }
+        GUI.TextArea(new Rect(250, 100, 300, 300), _messageLog);
+    }
+    [RPC]
+    void OnConnectedToServer(){
+    	AddMessage("Connected to the server.");
+    }
+    void OnDisconnectedToServer(){
+    	AddMessage("Disconnected from the server.");
+    }
+    void OnFailedToConnect(NetworkConnectionError error){
+    	AddMessage("Didn't connect because: "+error);
+    }
+    [RPC]
+    void StartLogin(NetworkPlayer player){
+        AddMessage("hahahahahahahahah yay login");
+    }
+    [RPC]
+    void SendInfoToServer(){
+        AddMessage("Sending stuff to server!");
+        someInfo = "Client " + _myNetworkPlayer.guid + ": hello server";
+        networkView.RPC("ReceiveInfoFromClient", RPCMode.Server, someInfo);
+        AddMessage ("SENT: "+someInfo);
+    }
+    [RPC]
+    void SetPlayerInfo(NetworkPlayer player) {
+        _myNetworkPlayer = player;
+        someInfo = "Player setted";
+        networkView.RPC("ReceiveInfoFromClient", RPCMode.Server, someInfo);
+        AddMessage ("SENT: "+someInfo);
+    }
+    [RPC]
+    void ReceiveInfoFromServer(string someInfo) {
+        AddMessage("GOT: "+someInfo);
+    }
+    void AddMessage(string message){
+        _messageLog += message + "\n";
+    }
+ 
+    // fix RPC errors
+    [RPC]
+    void ReceiveInfoFromClient(string someInfo) {
+	}
+    [RPC]
+    void SendInfoToClient(string someInfo) {
+    }
+}
