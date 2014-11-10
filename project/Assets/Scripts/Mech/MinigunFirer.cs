81df6cd37a25fa04ba57c35f7abe3f74bc4e65a3
christianFerreira <cf3rreira@gamil.com>
2014-11-10 15:52:16 -0500

    fixed it i think?

--- project/Assets/Scripts/Mech/MinigunFirer.cs
+++ project/Assets/Scripts/Mech/MinigunFirer.cs
@@ -6,16 +6,19 @@ public class MinigunFirer : MonoBehaviour {
 	public GameObject sparkPrefab;
 	public bool fire;
 	public float range = 100.0f;
-	public float coolDown = 0.5f;
+	public float coolDown = 0.1f;
 	float cooldownRemaining = 0;
 
 	//ammo variables for minigun
 	public float gunClipAmmo = 40f;
 	public float currentClipAmmo = 40f;
 
+	int layerMask = 1 << 16;
+
 	// Use this for initialization
 	void Start () {
 		fire = false;
+		layerMask = ~layerMask;
 	}
 	
 	// Update is called once per frame
@@ -29,29 +32,32 @@ public class MinigunFirer : MonoBehaviour {
 
 		if (currentClipAmmo >= 1 && fire == true && cooldownRemaining <= 0) {
 
-			//add some form of recoil or bullet spread when firing ****************************
-
-			Ray ray = new Ray (gameObject.transform.position, gameObject.transform.forward);
+			//gets the starting aimer angle
+			Vector3 tempStart = gameObject.transform.forward;
+			//ads a randoma mount of spread to the angle
+			Vector3 startShot =  tempStart += new Vector3 (Random.Range (-0.02F, 0.02F), Random.Range (-0.02F, 0.02F), Random.Range (-0.02F, 0.02F));
+			Ray ray = new Ray (gameObject.transform.position, startShot);
 			RaycastHit hitInfo;
-			if (Physics.Raycast (ray, out hitInfo, range)) {
-				Vector3 hitPoint = hitInfo.point;
-				GameObject go = hitInfo.collider.gameObject;
+			//fires the adjusted ray
+			if (Physics.Raycast (ray, out hitInfo, range,layerMask)) {
+					Vector3 hitPoint = hitInfo.point;
+					GameObject go = hitInfo.collider.gameObject;
 
-				/*objectHealth h = go.GetComponent<objectHealth>();
+					/*objectHealth h = go.GetComponent<objectHealth>();
 
-				if(h != null)
-				{
-				h.ReciveDamage(damage);
-				}*/
-				
-				// applies bullet spark to location fo impact
-				if (sparkPrefab != null) {
-					Instantiate (sparkPrefab, hitPoint, Quaternion.identity);
+					if(h != null)
+					{
+					h.ReciveDamage(damage);
+					}*/
 					
-				}
-				//lower bullets whenever a shot is taken
-				currentClipAmmo -=1;
-				cooldownRemaining = coolDown;
+					// applies bullet spark to location fo impact
+					if (sparkPrefab != null) {
+						Instantiate (sparkPrefab, hitPoint, Quaternion.identity);
+						
+					}
+					//lower bullets whenever a shot is taken
+					currentClipAmmo -=1;
+					cooldownRemaining = coolDown;
 			}
 		}
 	}
