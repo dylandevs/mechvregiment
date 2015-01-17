5c561ed728812bbc0cc3fb626a1b5980a69c0ab4
dylan <dwyllace@gmail.com>
2015-01-17 17:05:22 -0500

    Added in Photon plugin to experimental Goliath project Updated Goliath networking script to use Photon, still needs component in scene.

--- experimental/Goliath/Assets/Scripts/PlayerNetSend.cs
+++ experimental/Goliath/Assets/Scripts/PlayerNetSend.cs
@@ -1,60 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerNetSend : MonoBehaviour {
+public class PlayerNetSend : Photon.MonoBehaviour {
 
-	private string serverIP = "192.168.56.1";
-	private int port = 25000;
-	private string gameName = "GoliathConnection_083";
-	private string gameType = "MechvRegimentMatch";
+    private string roomName = "GoliathConnection_083";
 
-	float countdownTimer = 5;
-	bool stop = false;
-	bool startCount = false;
+	//float countdownTimer = 5;
 
 	public GameObject goliathTop;
 	public GameObject goliathBot;
 
 	// Use this for initialization
 	void Start () {
-		if (Network.peerType == NetworkPeerType.Disconnected){
-			/*bool useNat = !Network.HavePublicAddress();
-			if(useNat){
-				print("Network has no private address; NAT punching.");
-			}*/
-			Network.InitializeServer(10, port, !Network.HavePublicAddress());
-			MasterServer.RegisterHost(gameType, gameName, "This is the mech v regiment connection");
-		}
+		PhotonNetwork.ConnectUsingSettings("v4.2");
 	}
+    void MakeRoom(){
+        RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 2 };
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (startCount){
-			countdownTimer -= Time.deltaTime;
-		}
-		if (countdownTimer <= 0){
-			countdownTimer = 5;
-			print ("called");
-			stop = true;
-			MasterServer.RequestHostList(gameType);
-			HostData[] hostList = MasterServer.PollHostList();
-			print (hostList.Length);
-			for(int i = 0; i < hostList.Length; i++){
-				print (hostList[i].gameName);
-				print("IP: " + hostList[i].ip[0] + " Port: "+ hostList[i].port);
-			}
-		}
+		Debug.Log(PhotonNetwork.connectionStateDetailed.ToString());
+        if(PhotonNetwork.connectionStateDetailed.ToString() == "JoinedLobby"){
+            MakeRoom();
+        }
+        else {
+        	//Everything for post joining here
+        }
 	}
-
-	void OnServerInitialized(){
-		
-	}
-	void OnMasterServerEvent(MasterServerEvent mse){
-		if (mse == MasterServerEvent.RegistrationSucceeded){
-			print ("regSucceed");
-			startCount = true;
-		}
-	}
+    void OnJoinedRoom(){
+        Debug.Log("Joined room!");
+    }
 
 	[RPC]
 	void UpdateNathanPos(Vector3 pos){
