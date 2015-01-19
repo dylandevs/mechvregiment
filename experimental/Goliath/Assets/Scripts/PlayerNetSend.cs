c0e1231b62caf7e7a8e6244716508d5f4b151507
Nathan <jjchan31@hotmail.com>
2015-01-19 14:52:22 -0500

    Reverted to Photon...I think?

--- experimental/Goliath/Assets/Scripts/PlayerNetSend.cs
+++ experimental/Goliath/Assets/Scripts/PlayerNetSend.cs
@@ -1,37 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerNetSend : Photon.MonoBehaviour {
+public class PlayerNetSend : MonoBehaviour {
 
-    private string roomName = "GoliathConnection_083";
+	private string serverIP = "192.168.56.1";
+	private int port = 25000;
+	private string gameName = "GoliathConnection_083";
+	private string gameType = "MechvRegimentMatch";
 
-	//float countdownTimer = 5;
+	float countdownTimer = 5;
+	bool stop = false;
+	bool startCount = false;
 
 	public GameObject goliathTop;
 	public GameObject goliathBot;
 
 	// Use this for initialization
 	void Start () {
-		PhotonNetwork.ConnectUsingSettings("v4.2");
+		if (Network.peerType == NetworkPeerType.Disconnected){
+			/*bool useNat = !Network.HavePublicAddress();
+			if(useNat){
+				print("Network has no private address; NAT punching.");
+			}*/
+			Network.InitializeServer(10, port, !Network.HavePublicAddress());
+			MasterServer.RegisterHost(gameType, gameName, "This is the mech v regiment connection");
+		}
 	}
-    void MakeRoom(){
-        RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 2 };
-        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
-    }
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(PhotonNetwork.connectionStateDetailed.ToString());
-        if(PhotonNetwork.connectionStateDetailed.ToString() == "JoinedLobby"){
-            MakeRoom();
-        }
-        else {
-        	//Everything for post joining here
-        }
+		if (startCount){
+			countdownTimer -= Time.deltaTime;
+		}
+		if (countdownTimer <= 0){
+			countdownTimer = 5;
+			print ("called");
+			stop = true;
+			MasterServer.RequestHostList(gameType);
+			HostData[] hostList = MasterServer.PollHostList();
+			print (hostList.Length);
+			for(int i = 0; i < hostList.Length; i++){
+				print (hostList[i].gameName);
+				print("IP: " + hostList[i].ip[0] + " Port: "+ hostList[i].port);
+			}
+		}
 	}
-    void OnJoinedRoom(){
-        Debug.Log("Joined room!");
-    }
+
+	void OnServerInitialized(){
+		
+	}
+	void OnMasterServerEvent(MasterServerEvent mse){
+		if (mse == MasterServerEvent.RegistrationSucceeded){
+			print ("regSucceed");
+			startCount = true;
+		}
+	}
 
 	[RPC]
 	void UpdateNathanPos(Vector3 pos){
