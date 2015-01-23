73c6f394627ee5ead42932e2306a9b6e423cd4c9
dylan <dwyllace@gmail.com>
2015-01-23 00:57:30 -0500

    Fixed photon view initialization in the player networking script

--- experimental/Goliath/Assets/Scripts/PlayerNetSend.cs
+++ experimental/Goliath/Assets/Scripts/PlayerNetSend.cs
@@ -5,7 +5,7 @@ public class PlayerNetSend : Photon.MonoBehaviour {
 
     private string roomName = "GoliathConnection_083";
 
-	PhotonView photonView = PhotonView.Get(this);
+	PhotonView photonView;
             
 
 	public GameObject goliathTop;
@@ -14,6 +14,7 @@ public class PlayerNetSend : Photon.MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		PhotonNetwork.ConnectUsingSettings("v4.2");
+		photonView = PhotonView.Get(this)
 	}
 	void MakeRoom(){
         RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 2 };
