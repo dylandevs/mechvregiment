165ceecee571d3435812dd763064e2ff514942bc
dylan <dwyllace@gmail.com>
2015-02-09 15:15:01 -0500

    Removed old networking script. Removed extra photon settings file Added new terrain shared prefab Added player functions to goliath networking script

--- project/Assets/Scripts/Goliath/GoliathNetworking.cs
+++ project/Assets/Scripts/Goliath/GoliathNetworking.cs
@@ -10,52 +10,58 @@ public class GoliathNetworking : MonoBehaviour {
 	public GameObject GoliathTop;
 	public GameObject GoliathBottom;
 
-	// Use this for initialization
+    public GameObject Player1Avatar;
+    public GameObject Player2Avatar;
+    public GameObject Player3Avatar;
+    public GameObject Player4Avatar;
+
+//INITIALIZATION
+
 	void Start () {
 		PhotonNetwork.ConnectUsingSettings("v4.2");
 		photonView = PhotonView.Get (this);
 	}
-	void MakeRoom(){
-        RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 2 };
-        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
-    }
 	
-	// Update is called once per frame
+//UPDATE
+
 	void Update () {
 		sendTimer -= Time.deltaTime;
 
 		if(PhotonNetwork.connectionStateDetailed.ToString() == "JoinedLobby"){
+            //Once connected to Photon, join the room or make the room.
             Debug.Log(PhotonNetwork.connectionStateDetailed.ToString());
             MakeRoom();
         }
         else if(PhotonNetwork.connectionStateDetailed.ToString() == "Joined"){
-        	//Code here happens when lobby is all set up
-        	//Photon RPC example
-        	//photonView.RPC("SendInfoToServer", PhotonTargets.All, "hhhhe he he hi");
-			if(sendTimer <=0){
+        	if(sendTimer <= 0){
+                //Here's where the RPC calls go so they happen once properly joined.
+
         		SendGoliathTransforms(GoliathTop.transform, GoliathBottom.transform);
-				sendTimer = 0.05f;
+
+                sendTimer = 0.05f;
 			}
         }
         else {
         	Debug.Log(PhotonNetwork.connectionStateDetailed.ToString());
         }
 	}
+
+//CONNECTING / NETWORKING
+
+    void MakeRoom(){
+        RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 2 };
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
 	private void OnPhotonJoinRoomFailed (){
         Debug.Log("Photon failed to join room.");
     }
-
-    void OnJoinedRoom(){
+    private void OnJoinedRoom(){
     	Room currentRoom = PhotonNetwork.room;
     	Debug.Log("Room \""+ currentRoom.name +"\" has this many joined: " + currentRoom.playerCount);
     }
 
-    private Transform DecerealizeTransform(Vector3 position, Quaternion rotation){
-    	GameObject outputObject = new GameObject();
-    	outputObject.transform.position = position;
-    	outputObject.transform.rotation = rotation;
-    	return outputObject.transform;
-    }
+
+//RPC CALLS
 
     public void SendGoliathTransforms(Transform Top, Transform Bottom){
     	photonView.RPC("ExchangeGoliathPositioning", PhotonTargets.All, Top.position, Top.rotation, Bottom.position, Bottom.rotation);
@@ -63,5 +69,26 @@ public class GoliathNetworking : MonoBehaviour {
     [RPC]
     public void ExchangeGoliathPositioning(Vector3 topPos, Quaternion topRot, Vector3 botPos, Quaternion botRot){
     	print("I'm sending the position over");
+    }
+
+    [RPC]
+    public void PositionPlayer1(Vector3 newPos, Quaternion newRot){
+        Player1Avatar.transform.position = newPos;
+        Player1Avatar.transform.rotation = newRot;
+    }
+    [RPC]
+    public void PositionPlayer2(Vector3 newPos, Quaternion newRot){
+        Player2Avatar.transform.position = newPos;
+        Player2Avatar.transform.rotation = newRot;
+    }
+    [RPC]
+    public void PositionPlayer3(Vector3 newPos, Quaternion newRot){
+        Player3Avatar.transform.position = newPos;
+        Player3Avatar.transform.rotation = newRot;
+    }
+    [RPC]
+    public void PositionPlayer4(Vector3 newPos, Quaternion newRot){
+        Player4Avatar.transform.position = newPos;
+        Player4Avatar.transform.rotation = newRot;
     }
 }
