10243715c1ddaef853988163d9a0b4def816ee79
Nathan <jjchan31@hotmail.com>
2015-02-09 20:38:20 -0500

    Began some setup for minimap notification manager and object pooling (Scavenger).

---
+++ project/Assets/Scripts/PoolManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoolManager : MonoBehaviour {
+
+	public int startingPool = 1;
+	public bool loopable = false;
+	public GameObject prefabObject;
+
+	private GameObject[] objectPool;
+	private GameObject[] inactiveObjectPool;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
