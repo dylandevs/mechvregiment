17d1de6c9532bddf649521825eaa2faccb5088c1
Nathan <jjchan31@hotmail.com>
2015-03-12 17:40:34 -0400

    Added correct damager script to player avatars.

---
+++ project/Assets/Scripts/PlayerAvatarDamager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAvatarDamager : MonoBehaviour {
+
+	public PlayerAvatar player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void DamagePlayer(float damage, Vector3 direction){
+		player.Damage(damage, direction);
+	}
+}
