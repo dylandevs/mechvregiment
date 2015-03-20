2b2ac911e878c5b11876bc49b61e7863c927ec86
Nathan <jjchan31@hotmail.com>
2015-03-20 17:58:34 -0400

    Updated Scav FPS model, some animations still need work.

--- project/Assets/Scripts/Scavenger/ScavGame.cs
+++ project/Assets/Scripts/Scavenger/ScavGame.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ScavGame : MonoBehaviour {
 
+	public float StartMatchTime = 300;
+	public UnityEngine.UI.Text timerText;
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
