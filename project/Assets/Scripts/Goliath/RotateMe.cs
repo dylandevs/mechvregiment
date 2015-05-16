489ed4c4431d4d3883bf0f50b69e9bf2e8eb96b4
dylan <dwyllace@gmail.com>
2015-05-16 19:32:52 -0400

    New AI target reticule

---
+++ project/Assets/Scripts/Goliath/RotateMe.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateMe : MonoBehaviour {
+
+	public float speedX;
+	public float speedY;
+	public float speedZ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(speedX*Time.deltaTime, speedY*Time.deltaTime, speedZ*Time.deltaTime);
+	}
+}
